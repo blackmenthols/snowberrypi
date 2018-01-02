c519e31f5c61ed31b7ada86089d5ae97e5ef4136
Snowberry Pi <snowberry@foo.com>
2018-01-02 03:47:09 +0000

    blah blah

--- led.py
+++ led.py
@@ -1,45 +1,93 @@
 #!/usr/bin/python
-
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 import os
+import gpiozero
+from gpiozero import LED, PWMLED, Button
+from signal import pause
+import subprocess, shlex
+from multiprocessing import Process
 
-def on_18():
-	GPIO.output(18,GPIO.HIGH)
-	GPIO.output(23,GPIO.LOW)
-	GPIO.output(24,GPIO.LOW)
-	time.sleep(.300)
+gpio_leds = [18,23,24]
+gpio_btn = 2
 
-def on_23():
-	GPIO.output(23,GPIO.HIGH)
-	GPIO.output(18,GPIO.LOW)
-	GPIO.output(24,GPIO.LOW)
-	time.sleep(.300)
-
-def on_24():
-	GPIO.output(24,GPIO.HIGH)
-	GPIO.output(23,GPIO.LOW)
-	GPIO.output(18,GPIO.LOW)
-	time.sleep(.300)
+def setup_all():
+	for pins in gpio_leds:
+		GPIO.setup(pins, GPIO.OUT, initial=False)
 
 def all_on():
-	on_18()
-	on_23()
-	on_24()
+	for pins in gpio_leds:
+		GPIO.output(pins, GPIO.HIGH)
 
+def all_off():
+	for pins in gpio_leds:
+		GPIO.output(pins, GPIO.LOW)
 
-GPIO.setwarnings(False)
+# first one on, the rest off
+def sequence_on(first, second, third):
+	GPIO.output(first,GPIO.HIGH)
+	GPIO.output(second,GPIO.LOW)
+	GPIO.output(third,GPIO.LOW)
+
+def run_long_sequence():
+	setup_all()
+	for i in range(0, 4):
+		print i
+		sequence_on(18,23,24)
+		sleep(1)
+		sequence_on(24,23,18)
+		sleep(1)
+		sequence_on(23,24,18)
+		sleep(1)
+		all_off()
+
+def run_omx():
+	subprocess.call(shlex.split("omxplayer -o local /home/pi/meep.mp3 --vol -3000"))
+
+def hacky():
+	worker1 = Process(name='run_long_sequence', target=run_long_sequence)
+	worker2 = Process(name='run_omx', target=run_omx)
+	worker1.start()
+	worker1.join()
+	worker2.start()
+	worker2.join()
+	return None
+
+# regular setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(24,GPIO.OUT)
+GPIO.setwarnings(False)
 
+# button stuffs
+GPIO.setup(gpio_btn, GPIO.IN)
+GPIO.add_event_detect(gpio_btn, GPIO.RISING)
+hacky()
+GPIO.cleanup()
+exit(1)
+
+#if GPIO.event_detected(gpio_btn):
+#	print('Button pressed')
+
+'''
+button.wait_for_press()
+button.when_pressed=sequence_on(18,23,24)
+sleep(2)
+print "Pressed! 1"
+
+button.wait_for_press()
+button.when_pressed=all_off()
+sleep(2)
+print "Pressed! 0"
+
+GPIO.setmode(GPIO.BCM)
+button.wait_for_press()
+button.when_pressed=hacky()
+print "Pressed! meep"
+exit(1)
+'''
+
+'''
 for x in range(0, 30):
 	all_on()
 	print x
 
-GPIO.cleanup()
-
-
-
-
+'''
